97aa36f232b95cde0f729b90d7bf8be1c175a4f4
pavelflorianovich <55707243+pavelflorianovich@users.noreply.github.com>
2019-10-19 23:18:11 +0300

    Add files via upload

--- e4.py
+++ e4.py
@@ -1,11 +1,8 @@
-list1 = []
-
-
 def mogno(dct, maxi):
+    list1 = []
     for key in dct:
         if dct[key] <= maxi:
             list1.append(key)
     return list1
 
 
-mogno({'banana': 10, 'apple': 500, 'dog': 34, 'steak': 100}, 5)
--- e5.py
+++ e5.py
@@ -1,7 +1,7 @@
 def unicum(list1):
     set1 = set(list1)
-    list(set1)
-    return set1
+    list1 = list(set1)
+    return list1
 
 
-unicum([1, 1, 1, 0, 3, 32])
+
